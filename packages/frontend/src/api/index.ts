479a583cd794df809b7c2da217759a4349beeae7
Mia <vottus@vott.us>
2021-06-21 17:47:27 +0200

    fix: vite_api_url

--- packages/frontend/src/api/index.ts
+++ packages/frontend/src/api/index.ts
@@ -3,5 +3,10 @@ import axios from "axios";
 export const $http = axios.create({
   // TODO: fixup for prod
   // TODO: better handling of DEV_URL
-  baseURL: import.meta.env.VITE_API_URL ?? import.meta.env.DEV ? "http://127.0.0.1:4200/api" : "http://euphoria.vott.us",
+  baseURL:
+    (typeof import.meta.env.VITE_API_URL === "string"
+      ? import.meta.env.VITE_API_URL
+      : import.meta.env.DEV
+      ? "http://127.0.0.1:4200/api"
+      : "http://euphoria.vott.us/api") as string,
 });
