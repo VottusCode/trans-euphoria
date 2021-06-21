900c1987005caa967f297a880b0c7ae9c9da11d3
Mia <vottus@vott.us>
2021-06-21 17:33:27 +0200

    feat: add env var for frontend api url

--- packages/frontend/src/api/index.ts
+++ packages/frontend/src/api/index.ts
@@ -3,5 +3,5 @@ import axios from "axios";
 export const $http = axios.create({
   // TODO: fixup for prod
   // TODO: better handling of DEV_URL
-  baseURL: "http://127.0.0.1:4200/api",
+  baseURL: import.meta.env.VITE_API_URL ?? import.meta.env.DEV ? "http://127.0.0.1:4200/api" : "http://euphoria.vott.us",
 });
